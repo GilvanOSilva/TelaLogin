48886c7ea3f51625a433586d1075289233400c38
Gilvan Oliveira Silva <115108137+GilvanOSilva@users.noreply.github.com>
2022-12-20 16:58:43 -0300

    Corrigido mensagem de sistema que persistiam quando o usuário mudava de tela.

--- TelaLogin.py
+++ TelaLogin.py
@@ -3,7 +3,7 @@ from openpyxl import load_workbook
 from time import sleep
 
 
-class LoginSys(Tk):
+class UserSys(Tk):
     def __init__(self, *args, **kargs):
         Tk.__init__(self, *args, **kargs)
         screen = Frame(self)
@@ -19,9 +19,12 @@ class LoginSys(Tk):
     def show_frame(self, page_name):
         frame = self.frames[page_name]
         frame.tkraise()
+        frame.label_message['text'] = ''
 
 
 class Login(Frame):
+    label_message = None
+
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.controller = controller
@@ -29,8 +32,6 @@ class Login(Frame):
         font_size = 5
         username = StringVar()
         password = StringVar()
-        username.set('user01')
-        password.set('variant01')
         label_user = Label(self, text='Username', padx=pad_size, pady=pad_size, justify='left', font=font_size)
         label_user.grid(column=0, row=0, sticky='W')
         entry_user = Entry(self, textvariable=username, font=font_size)
@@ -40,44 +41,44 @@ class Login(Frame):
         entry_password = Entry(self, show='*', textvariable=password, font=font_size)
         entry_password.grid(column=1, row=1)
         button_login = Button(self, text='Acessar', command=lambda: self.access(entry_user.get(), entry_password.get()), padx=pad_size, pady=pad_size, font=font_size)
-        button_login.grid(column=1, row=3, sticky='E , W')
+        button_login.grid(column=1, row=2, sticky='E , W')
         button_new_pass = Button(self, text='Esqueci a senha', command=lambda: controller.show_frame('ResetPSW'), padx=pad_size, pady=pad_size, font=font_size)
-        button_new_pass.grid(column=1, row=4, sticky='E , W')
+        button_new_pass.grid(column=1, row=3, sticky='E , W')
         button_registry = Button(self, text='Cadastrar Novo Username', command=lambda: controller.show_frame('Registry'), padx=pad_size, pady=pad_size, font=font_size)
-        button_registry.grid(column=1, row=5, sticky='E , W')
+        button_registry.grid(column=1, row=4, sticky='E , W')
         button_quit = Button(self, text='Sair', command=quit, padx=pad_size, pady=pad_size, font=font_size)
-        button_quit.grid(column=1, row=6, sticky='E , W')
-        Login.label_message = Label(self, text='', padx=pad_size, pady=pad_size, justify='left', font=font_size)
-        Login.label_message.grid(column=1, row=7, sticky='W')
+        button_quit.grid(column=1, row=5, sticky='E , W')
+        Login.label_message = Label(self, text='', padx=pad_size, pady=pad_size, justify='left', relief='sunken', font=font_size)
+        Login.label_message.grid(column=1, row=6, sticky='EW')
 
     def access(self, username, password):
         user_pos = globals()
-        file_xl = load_workbook(filename='Logins.xlsx')
-        logins = file_xl.active
+        file_xl = load_workbook(filename='Users.xlsx')
+        users = file_xl.active
         pos_tab = 1
-        while logins['A' + str(pos_tab)].value is not None:
-            if username == logins['A' + str(pos_tab)].value:
-                if password == logins['B' + str(pos_tab)].value:
-                    user_pos['USER'] = pos_tab
-                    Update.password.set(logins['B' + str(USER)].value)
-                    Update.name.set(logins['D' + str(USER)].value)
-                    Update.age.set(logins['E' + str(USER)].value)
-
-                    ControlPanel.label_welcome['text'] = 'Bem Vindo ' + logins['A' + str(pos_tab)].value
-                    Remove.label_aviso['text'] = ControlPanel.label_welcome['text']
-                    Login.label_message['text'] = 'Obtido acesso.'
-                    sleep(1)
-                    LoginSys.show_frame(self.controller, 'ControlPanel')
+        while users['A' + str(pos_tab)].value is not None:
+            if username == users['A' + str(pos_tab)].value:
+                if password == users['B' + str(pos_tab)].value:
+                    user_pos['POS'] = pos_tab
+                    Update.password.set(users['B' + str(POS)].value)
+                    Update.name.set(users['D' + str(POS)].value)
+                    Update.age.set(users['E' + str(POS)].value)
+                    ControlPanel.label_welcome['text'] = 'Bem Vindo ' + users['A' + str(pos_tab)].value
+                    Remove.label_welcome['text'] = ControlPanel.label_welcome['text']
+                    sleep(0.5)
+                    UserSys.show_frame(self.controller, 'ControlPanel')
                     break
                 else:
                     Login.label_message['text'] = 'Senha incorreta.'
                     break
             pos_tab += 1
-        if logins['A' + str(pos_tab)].value is None:
+        if users['A' + str(pos_tab)].value is None:
             Login.label_message['text'] = 'User não está cadastrado.'
 
 
 class Registry(Frame):
+    label_message = None
+
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.controller = controller
@@ -112,47 +113,45 @@ class Registry(Frame):
         button_registry.grid(column=1, row=5, sticky='N , S, W, E')
         button_return = Button(self, text='Voltar', command=lambda: controller.show_frame('Login'), padx=pad_size, pady=pad_size, font=font_size)
         button_return.grid(column=1, row=6, sticky='N , S, W, E')
-        Registry.label_message = Label(self, text='', padx=pad_size, pady=pad_size, justify='left', font=font_size)
-        Registry.label_message.grid(column=1, row=7, rowspan=2, sticky='N , S')
+        Registry.label_message = Label(self, text='', padx=pad_size, pady=pad_size, justify='left', relief='sunken', font=font_size)
+        Registry.label_message.grid(column=1, row=7, sticky='EW')
 
     def registry_user(self, username, password, email, name, age):
-        file_xl = load_workbook(filename='Logins.xlsx')
-        logins = file_xl.active
+        file_xl = load_workbook(filename='Users.xlsx')
+        users = file_xl.active
         pos_tab = 1
         exist_user = False
         exist_email = False
-        while logins['A' + str(pos_tab)].value is not None:
-            if logins['A' + str(pos_tab)].value == username:
+        while users['A' + str(pos_tab)].value is not None:
+            if users['A' + str(pos_tab)].value == username:
                 exist_user = True
-            if logins['C' + str(pos_tab)].value == email:
+            if users['C' + str(pos_tab)].value == email:
                 exist_email = True
             pos_tab += 1
         if exist_user is True:
             Registry.label_message['text'] = 'Este usuário já está cadastrado.'
-            print()
         elif len(username) < 6 or ' ' in username:
             Registry.label_message['text'] = 'Usuário não atende o crítério.'
-            print()
         elif exist_email is True:
             Registry.label_message['text'] = 'Este E-mail já está cadastrado.'
-            print()
         elif '@' not in email or '.' not in email or ' ' in email:
             Registry.label_message['text'] = 'E-mail não atende o crítério.'
-            print()
         else:
             pos_tab = 1
-            while logins['A' + str(pos_tab)].value is not None:
+            while users['A' + str(pos_tab)].value is not None:
                 pos_tab += 1
-            logins['A' + str(pos_tab)] = username
-            logins['B' + str(pos_tab)] = password
-            logins['C' + str(pos_tab)] = email
-            logins['D' + str(pos_tab)] = name
-            logins['E' + str(pos_tab)] = age
-            file_xl.save(filename='Logins.xlsx')
+            users['A' + str(pos_tab)] = username
+            users['B' + str(pos_tab)] = password
+            users['C' + str(pos_tab)] = email
+            users['D' + str(pos_tab)] = name
+            users['E' + str(pos_tab)] = age
+            file_xl.save(filename='Users.xlsx')
             Registry.label_message['text'] = 'Cadastro efetuado com sucesso.'
 
 
 class ResetPSW(Frame):
+    label_message = None
+
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.controller = controller
@@ -174,37 +173,39 @@ class ResetPSW(Frame):
         button_submit.grid(column=1, row=4, sticky='N , S, W, E')
         button_return = Button(self, text='Voltar', command=lambda: controller.show_frame('Login'), padx=pad_size, pady=pad_size, font=font_size)
         button_return.grid(column=1, row=5, sticky='N , S, W, E')
-        ResetPSW.label_message = Label(self, text='', padx=pad_size, pady=pad_size, justify='left', font=font_size)
-        ResetPSW.label_message.grid(column=0, columnspan=2, row=6, sticky='N , S')
+        ResetPSW.label_message = Label(self, text='', padx=pad_size, pady=pad_size, justify='left', relief='sunken', font=font_size)
+        ResetPSW.label_message.grid(column=1, row=6, sticky='EW')
 
     def reset_user_psw(self, username, email):
-        file_xl = load_workbook(filename='Logins.xlsx')
-        logins = file_xl.active
+        file_xl = load_workbook(filename='Users.xlsx')
+        users = file_xl.active
         pos_tab = 1
-        while logins['A' + str(pos_tab)].value is not None:
-            if logins['A' + str(pos_tab)].value == username:
-                if logins['C' + str(pos_tab)].value == email:
-                    logins['B' + str(pos_tab)] = 'Senha12345'
-                    file_xl.save(filename='Logins.xlsx')
+        while users['A' + str(pos_tab)].value is not None:
+            if users['A' + str(pos_tab)].value == username:
+                if users['C' + str(pos_tab)].value == email:
+                    users['B' + str(pos_tab)] = 'Senha12345'
+                    file_xl.save(filename='Users.xlsx')
                     ResetPSW.label_message['text'] = 'Sua senha é, Senha12345, clique em voltar.'
                     break
                 else:
                     ResetPSW.label_message['text'] = 'E-mail inválido.'
                     break
             pos_tab += 1
-        if logins['A' + str(pos_tab)].value is None:
+        if users['A' + str(pos_tab)].value is None:
             ResetPSW.label_message['text'] = 'User não foi encontrado.'
-            print('User não foi encontrado')
 
 
 class ControlPanel(Frame):
+    label_message = None
+    label_welcome = None
+
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.controller = controller
-        global USER
         pad_size = 5
         font_size = 5
-        contagem_total = 4
+        contagem_total = ControlPanel.total_users(self)
+        ControlPanel.label_message = Label(self, text='')
         ControlPanel.label_welcome = Label(self, text='', padx=pad_size, pady=pad_size, justify='left', font=font_size)
         ControlPanel.label_welcome.grid(column=0, row=0, sticky='W')
         button_update = Button(self, text='Alterar dados da conta', command=lambda: controller.show_frame('Update'), padx=pad_size, pady=pad_size, font=font_size)
@@ -214,10 +215,26 @@ class ControlPanel(Frame):
         button_return = Button(self, text='Encerrar Sessão', command=lambda: controller.show_frame('Login'), padx=pad_size, pady=pad_size, font=font_size)
         button_return.grid(column=0, row=4, sticky='N , S, W, E')
         label_total_user = Label(self, text='Total de users cadastrados: ' + str(contagem_total), padx=pad_size, pady=pad_size, justify='left', font=font_size)
-        label_total_user.grid(column=0, row=5, sticky='W')
+        label_total_user.grid(column=0, row=5, sticky='EW')
+
+
+    def total_users(self):
+        file_xl = load_workbook(filename='Users.xlsx')
+        users = file_xl.active
+        pos_tab = 1
+        total = 0
+        while users['A' + str(pos_tab)].value is not None:
+            total += 1
+            pos_tab += 1
+        return total
 
 
 class Update(Frame):
+    name = None
+    age = None
+    label_message = None
+    password = None
+
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.controller = controller
@@ -242,49 +259,54 @@ class Update(Frame):
         button_update.grid(column=1, row=5, sticky='N , S, W, E')
         button_return = Button(self, text='Voltar', command=lambda: controller.show_frame('ControlPanel'), padx=pad_size, pady=pad_size, font=font_size)
         button_return.grid(column=1, row=6, sticky='N , S, W, E')
-        Update.label_message = Label(self, text='', padx=pad_size, pady=pad_size, justify='left', font=font_size)
-        Update.label_message.grid(column=1, row=7, rowspan=2, sticky='N , S')
+        Update.label_message = Label(self, text='', padx=pad_size, pady=pad_size, justify='left', relief='sunken', font=font_size)
+        Update.label_message.grid(column=1, row=7, sticky='EW')
 
     def update_user(self, password, name, age):
-        global USER
-        file_xl = load_workbook(filename='Logins.xlsx')
-        logins = file_xl.active
-        logins['B' + str(USER)] = password
-        logins['D' + str(USER)] = name
-        logins['E' + str(USER)] = age
-        file_xl.save(filename='Logins.xlsx')
+        global POS
+        file_xl = load_workbook(filename='Users.xlsx')
+        users = file_xl.active
+        users['B' + str(POS)] = password
+        users['D' + str(POS)] = name
+        users['E' + str(POS)] = age
+        file_xl.save(filename='Users.xlsx')
         Update.label_message['text'] = 'Dados alterados.'
 
 
 class Remove(Frame):
+    label_message = None
+    label_welcome = None
+
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.controller = controller
         pad_size = 5
         font_size = 5
-        Remove.label_aviso = Label(self, text='', padx=pad_size, pady=pad_size, justify='left', font=font_size)
-        Remove.label_aviso.grid(column=0, row=0, sticky='W')
+        Remove.label_message = Label(self, text='')
+        Remove.label_welcome = Label(self, text='', padx=pad_size, pady=pad_size, justify='left', font=font_size)
+        Remove.label_welcome.grid(column=0, row=0, sticky='W')
         label_message = Label(self, text='AVISO: Esta ação remove o seu user do sistema!', padx=pad_size, pady=pad_size, justify='left', font=font_size)
         label_message.grid(column=0, row=1, sticky='N , S')
         button_remove = Button(self, text='Remover', command=lambda: self.remove_user(), padx=pad_size, pady=pad_size, font=font_size)
         button_remove.grid(column=0, row=2, sticky='N , S, W, E')
         button_return = Button(self, text='Voltar', command=lambda: controller.show_frame('ControlPanel'), padx=pad_size, pady=pad_size, font=font_size)
-        button_return.grid(column=0, row=3, sticky='N , S, W, E')
+        button_return.grid(column=0, row=3, sticky='NSEW')
 
     def remove_user(self):
-        global USER
-        file_xl = load_workbook(filename='Logins.xlsx')
-        logins = file_xl.active
-        logins['A' + str(USER)] = None
-        logins['B' + str(USER)] = None
-        logins['C' + str(USER)] = None
-        logins['D' + str(USER)] = None
-        logins['E' + str(USER)] = None
-        file_xl.save(filename='Logins.xlsx')
-        LoginSys.show_frame(self.controller, 'Login')
+        global POS
+        file_xl = load_workbook(filename='Users.xlsx')
+        users = file_xl.active
+        users['A' + str(POS)] = None
+        users['B' + str(POS)] = None
+        users['C' + str(POS)] = None
+        users['D' + str(POS)] = None
+        users['E' + str(POS)] = None
+        file_xl.save(filename='Users.xlsx')
+        UserSys.show_frame(self.controller, 'Login')
 
 
-USER = 9
-prog = LoginSys()
+POS = None
+prog = UserSys()
+prog.title('UserSys')
 prog.geometry('400x300')
 prog.mainloop()
